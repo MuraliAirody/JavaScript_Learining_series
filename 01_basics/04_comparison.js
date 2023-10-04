de8c67a53bf6026155e69a3c926dfd88a89f6fdc
MuraliAirody <muraliairody@gmail.com>
2023-10-04 13:42:13 +0530

    comparison of data types

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,19 @@
+// console.log("2">0);
+
+// console.log("02">1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+/*
+  an equality check ==  and comparison  < > <= >= works differently
+  equality operator compare object with number
+  comparison operator convert object to number and treat null as a '0'
+*/
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+//strict comparison '==='
+
+console.log("2" === 2);
