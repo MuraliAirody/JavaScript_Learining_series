197d1145c94e5e67fdd5373be1b693c2e7dd232a
MuraliAirody <muraliairody@gmail.com>
2023-10-11 12:20:05 +0530

    Json basics

--- 02_basics/04_object.js
+++ 02_basics/04_object.js
@@ -48,4 +48,38 @@ console.log(obj1)
 const obj3 = {...obj1,...obj2}
 console.log(obj3);
 
+console.log(Object.keys(user_details.fullNames))
+console.log(Object.values(user_details))
 
+console.log(Object.entries(user_details));
+
+console.log(user_details.fullNames.fullUserName.hasOwnProperty("first_name"))
+
+
+const course={
+    name:"courseName",
+    price:100,
+    course_instructor:"courseLecture"
+}
+
+console.log(course.course_instructor); //instead of fetching course_instructor like this
+
+const {course_instructor} = course
+
+console.log(course_instructor); // we can also fetch like this by destructuring object
+
+const {course_instructor:C_I} = course
+
+console.log(C_I) // we can also fetching it by using alias name
+
+
+//JSON object look as bellow
+
+// {
+//     "name":"courseName",
+//     "price":100,
+//     "course_instructor":"courseLecture"
+// }
+
+//sample api format
+//https://randomuser.me/api/
