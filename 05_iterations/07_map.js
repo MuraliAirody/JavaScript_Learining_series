811736b7c6c8df13bb6fba8546c3010f1d313a90
MuraliAirody <muraliairody@gmail.com>
2023-11-13 11:54:52 +0530

    little modification

--- 05_iterations/07_map.js
+++ 05_iterations/07_map.js
@@ -1,11 +1,18 @@
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 const sqrt = myNums.map((item)=>Math.pow(item,2))
-console.log(sqrt);
+// console.log(sqrt);
+
+const newNums = myNums.map((items,index,array)=>{
+    console.log(items,index,array)
+    return items+1;
+})
+
+console.log(newNums);
 
 const nested = myNums
                .map((item)=>Math.sqrt(item))
                .map((item)=>Math.round(item))
                .filter((item)=>item>1)
 
-console.log(nested)               
+// console.log(nested)               
